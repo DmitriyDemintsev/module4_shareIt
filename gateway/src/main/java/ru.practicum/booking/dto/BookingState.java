7800f1bc97084aac60ce410fb7440fa8a888d6ed
DmitriyDemintsev <d.a.demintsev@yandex.ru>
2024-02-05 00:16:32 +0300

    скорерктирован enum BookingState

--- gateway/src/main/java/ru.practicum/booking/dto/BookingState.java
+++ gateway/src/main/java/ru.practicum/booking/dto/BookingState.java
@@ -3,18 +3,13 @@ package ru.practicum.booking.dto;
 import java.util.Optional;
 
 public enum BookingState {
-    // Все
-    ALL,
-    // Текущие
-    CURRENT,
-    // Будущие
-    FUTURE,
-    // Завершенные
-    PAST,
-    // Отклоненные
-    REJECTED,
-    // Ожидающие подтверждения
-    WAITING;
+
+    ALL, // Все
+    CURRENT, // Текущие
+    FUTURE, // Будущие
+    PAST, // Завершенные
+    REJECTED, // Отклоненные
+    WAITING; // Ожидающие подтверждения
 
     public static Optional<BookingState> from(String stringState) {
         for (BookingState state : values()) {
